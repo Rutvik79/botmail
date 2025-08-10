78624fadd0262dc795371ce95b56048d379b13e0
Rutvik Shinde <shinderutviksandeep@gmail.com>
2025-08-11 01:12:50 +0530

    fixed the address issue

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -5,7 +5,8 @@ import { db } from "@/server/db";
 export const POST = async (req: Request) => {
   const { data } = await req.json();
 
-  const emailAddress = data.email_addresses[0].emailAddress; // Replace with actual email extraction logic
+  const emailAddress =
+    data.email_addresses[0].emailAddress ?? "test@example.com"; // Replace with actual email extraction logic
   const firstName = data.first_name;
   const lastName = data.last_name;
   const imageUrl = data.profile_image_url;

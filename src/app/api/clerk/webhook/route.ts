b3ecc98fb5bfa0be65e9848fbdc24d55c46f78c6
Rutvik Shinde <shinderutviksandeep@gmail.com>
2025-08-11 02:00:12 +0530

    i dont know what i am doing

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,15 +1,18 @@
 // /api/clerk/webhook
 
 import { db } from "@/server/db";
-import { set } from "zod";
 
 export const POST = async (req: Request) => {
   const { data } = await req.json();
 
+  console.log("data", data);
+
+  console.log("hhi");
   const emailAddress =
     data.email_addresses && data.email_addresses.length > 0
-      ? data.email_addresses[0].emailAddress
+      ? data.email_addresses[0].email_address
       : "test@example.com";
+  console.log("debug", emailAddress);
   const firstName = data.first_name;
   const lastName = data.last_name;
   const imageUrl = data.profile_image_url;
@@ -24,9 +27,8 @@ export const POST = async (req: Request) => {
       imageUrl: imageUrl,
     },
   });
-
   console.log("User created in database:", { id, emailAddress });
-  await new Promise((r) => setTimeout(r, 0));
+
   return new Response("webhook received", {
     status: 200,
   });

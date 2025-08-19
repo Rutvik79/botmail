54e77af46147f30e513e2636cb564655af371e48
Rutvik Shinde <shinderutviksandeep@gmail.com>
2025-08-19 20:29:49 +0530

    Added the send button

--- src/app/mail/email-editor.tsx
+++ src/app/mail/email-editor.tsx
@@ -4,6 +4,8 @@ import StarterKit from "@tiptap/starter-kit";
 import { EditorContent, useEditor } from "@tiptap/react";
 import { Text } from "@tiptap/extension-text";
 import EditorMenuBar from "./editor-menubar";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 type Props = {};
 
@@ -35,14 +37,30 @@ const EmailEditor = (props: Props) => {
 
   return (
     <div>
-      <EditorMenuBar editor={editor} />
-      <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl w-full px-4">
-        <EditorContent editor={editor} value={value} />
+      <div className="flex border-b p-4 py-2">
+        <EditorMenuBar editor={editor} />
+      </div>
+      <div className="prose w-full p-4">
+        <EditorContent
+          editor={editor}
+          value={value}
+          placeholder="Write your email here..."
+        />
+      </div>
+
+      <Separator />
+      <div className="flex items-center justify-between px-4 py-3">
+        <span className="text-sm">
+          Tip: Press{" "}
+          <kbd className="rounded-lg border border-gray-200 bg-gray-100 px-2 py-1.5 text-xs font-semibold text-gray-800">
+            Cmd + J
+          </kbd>{" "}
+          for AI Autocomplete
+        </span>
+        <Button>Send</Button>
       </div>
     </div>
   );
 };
 
 export default EmailEditor;
-
-
